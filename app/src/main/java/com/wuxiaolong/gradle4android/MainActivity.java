2565cef7394bb1f13faecf1a19559ef70df7638d
WuXiaolong <wuxiaolong.me@gmail.com>
2017-03-13 11:21:35 +0800

    add

--- app/src/main/java/com/wuxiaolong/gradle4android/MainActivity.java
+++ app/src/main/java/com/wuxiaolong/gradle4android/MainActivity.java
@@ -24,10 +24,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                    Log.d("wxl", "API_SERVER_URL=" + BuildConfig.API_SERVER_URL);
             }
         });
 
-        Log.d("wxl", "API_SERVER_URL=" + BuildConfig.API_SERVER_URL);
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
